1bc9d47ded2a784694c6eddc5c77f22019ed625f
samrath-sudesh-acharya <samrathsacharya@gmail.com>
2022-04-23 23:42:15 +0530

    This commit consists of: - UI of the chatbot 	made with html,css,javascript - Crawler 	made with python and connected to elasticsearch running on 	docker

---
+++ chatbot-ui/script.js
@@ -0,0 +1,146 @@
+var sendForm = document.querySelector('#chatform'),
+    textInput = document.querySelector('.chatbox'),
+    chatList = document.querySelector('.chatlist'),
+    userBubble = document.querySelectorAll('.userInput'),
+    botBubble = document.querySelectorAll('.bot__output'),
+    animateBotBubble = document.querySelectorAll('.bot__input--animation'),
+    overview = document.querySelector('.chatbot__overview'),
+    hasCorrectInput,
+    imgLoader = false,
+    animationCounter = 1,
+    animationBubbleDelay = 600,
+    input,
+    previousInput,
+    isReaction = false,
+    unkwnCommReaction = "I didn't quite get that.",
+    chatbotButton = document.querySelector(".submit-button")
+
+sendForm.onkeydown = function(e){
+  if(e.keyCode == 13){
+    e.preventDefault();
+
+    //No mix ups with upper and lowercases
+    var input = textInput.value.toLowerCase();
+
+    //Empty textarea fix
+    if(input.length > 0) {
+      createBubble(input)
+    }
+  }
+};
+
+sendForm.addEventListener('submit', function(e) {
+  //so form doesnt submit page (no page refresh)
+  e.preventDefault();
+
+  //No mix ups with upper and lowercases
+  var input = textInput.value.toLowerCase();
+
+  //Empty textarea fix
+  if(input.length > 0) {
+    createBubble(input)
+  }
+}) //end of eventlistener
+
+var createBubble = function(input) {
+  //create input bubble
+  var chatBubble = document.createElement('li');
+  chatBubble.classList.add('userInput');
+
+  //adds input of textarea to chatbubble list item
+  chatBubble.innerHTML = input;
+
+  //adds chatBubble to chatlist
+  chatList.appendChild(chatBubble)
+
+  bot_response(input);
+}
+
+const bot_response = function(input){
+  url = "http://127.0.0.1:8000/search="
+  console.log(url.concat(input))
+  fetch(url.concat(input))
+  .then((data)=>{return data.json()})
+  .then((res)=>{create_response(res)})
+  
+}
+
+var create_response = function(text) {
+  var userBubble = document.createElement('li');
+  userBubble.classList.add('bot__output');
+  for(let i =0;i<text.length;i++){
+  userBubble.innerHTML = page_details(text[i])
+  chatList.appendChild(userBubble)
+  }
+  animationCounter = 1
+  textInput.value = "";
+}
+
+function page_details(text) {
+  responseText(`<h1 style="color:#27ae60">${truncate(text.title)}</h1><br/><p>${truncate(text.raw_text)}</p><br/><a href="${text.link}" target="_blank">View page</a>`)
+  return
+}
+
+function truncate(input) {
+  if (input.length > 200) {
+     return input.substring(0, 180) + '...';
+  }
+  return input;
+};
+
+
+function responseText(e) {
+
+  var response = document.createElement('li');
+
+  response.classList.add('bot__output');
+
+  //Adds whatever is given to responseText() to response bubble
+  response.innerHTML = e;
+
+  chatList.appendChild(response);
+
+  animateBotOutput();
+
+  console.log(response.clientHeight);
+
+  //Sets chatlist scroll to bottom
+  setTimeout(function(){
+    chatList.scrollTop = chatList.scrollHeight;
+    console.log(response.clientHeight);
+  }, 0)
+}
+
+function responseImg(e) {
+  var image = new Image();
+
+  image.classList.add('bot__output');
+  //Custom class for styling
+  image.classList.add('bot__outputImage');
+  //Gets the image
+  image.src = "/images/"+e;
+  chatList.appendChild(image);
+
+  animateBotOutput()
+  if(image.completed) {
+    chatList.scrollTop = chatList.scrollTop + image.scrollHeight;
+  }
+  else {
+    image.addEventListener('load', function(){
+      chatList.scrollTop = chatList.scrollTop + image.scrollHeight;
+    })
+  }
+}
+
+//change to SCSS loop
+function animateBotOutput() {
+  chatList.lastElementChild.style.animationDelay= (animationCounter * animationBubbleDelay)+"ms";
+  animationCounter++;
+  chatList.lastElementChild.style.animationPlayState = "running";
+}
+
+function commandReset(e){
+  animationCounter = 1;
+  previousInput = Object.keys(possibleInput)[e];
+}
+
---
+++ crawler/scrape.py
@@ -0,0 +1,81 @@
+from rich import print
+from turtle import color
+from rich.panel import Panel
+import requests
+import re
+import time
+import json
+from bs4 import BeautifulSoup
+from rich.console import Console
+from rich.table import Table
+from trafilatura import fetch_url, extract
+import os
+from dotenv import load_dotenv
+from rich.live import Live
+from elasticsearch import Elasticsearch
+
+load_dotenv()
+console = Console()
+
+#  Elastic configuration  
+
+es = Elasticsearch(os.getenv('ELASTICSEARCH_URL'),ca_certs=os.getenv('CA_CERT_LOCATION'),basic_auth=(os.getenv('USERNAME'),os.getenv('PASSWORD')))
+es.indices.create(index='page-data',ignore=400)
+
+# CONSTANTS
+
+urls = []
+data = []
+color = 'green'
+s_no = 0
+id = 0
+
+# Grab links from sitemap
+
+xml = requests.get('https://www.online.citibank.co.in/sitemap.xml')
+soup = BeautifulSoup(xml.content, 'lxml')
+xml_tag = soup.find_all('loc')
+for i in xml_tag:
+    s=i.text
+    url = re.sub('\\r\\n', '', s)
+    urls.append(url)
+
+# Get the response for the link
+
+for i in range(0,len(urls)):
+    res = requests.get(urls[i])
+    if res.status_code != 200:
+        color = 'red'
+    print(Panel(f"URL : [blue]{urls[i]}[/blue] || RESPONSE CODE : [{color}]{res.status_code}",width=100))
+
+    # Collecting data form the links 
+
+    page = BeautifulSoup(res.content,'lxml')
+    xml_tag = page.find_all('loc')
+    table = Table(show_header=True, header_style="bold magenta",width=150)
+    table.add_column("S.No",style="dim")
+    table.add_column("URL")
+    table.add_column("STATUS")
+    with Live(table, refresh_per_second=1):
+        for link in xml_tag:
+            status_color = 'green'
+            status = 'PASS'
+            s_no=s_no +1
+            time.sleep(2)
+            downloaded = fetch_url(link.text)
+            if downloaded is not None:
+                result = extract(downloaded,output_format='json',include_links=True)
+                if result is not None:
+                    id = id + 1
+                    es.index(index='page-data',document=result,id=id)
+                    data.append(json.loads(result))
+                else:
+                    status_color = 'red'
+                    status = 'FAIL'
+                with open("sample.json", "w",encoding='utf8') as outfile:
+                    json.dump(data, outfile, ensure_ascii=False)
+            else:
+                status_color = 'red'
+                status = 'FAIL'
+            table.add_row(f"{s_no}",f"{link.text}",f"[{status_color}]{status}")
+        
