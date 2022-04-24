701f6c31cf3f8342f0877fe0e0c6f6371d8f1d6b
Samrath Sudesh Acharya <76547134+samrath-sudesh-acharya@users.noreply.github.com>
2022-04-24 11:39:24 +0530

    Delete scarper directory

--- scarper/constant.py
+++
@@ -1,3 +0,0 @@
-COLOR:str = 'green'
-S_NO:int = 0
-ID:int = 0 
--- scarper/print.py
+++
@@ -1,8 +0,0 @@
-from requests import Response
-from rich import print 
-from constant import COLOR
-
-def print_response(response:Response):
-    if response.status_code != 200:
-        COLOR = 'red'
-        print()
